d3cbcbc85615418ff32a0a165aaa6b534501690d
Dtan713 <s.donaldtan@gmail.com>
2024-07-12 09:43:04 -0400

    some of glab30392

---
+++ src/Hospital.java
@@ -0,0 +1,11 @@
+public class Hospital {
+    public class Hospital {
+        public static void main(String args[]) {
+            Surgeon s = new Surgeon();
+            s.Doctor_Details();
+            s.Surgeon_Details();
+        }
+    }
+
+
+}
